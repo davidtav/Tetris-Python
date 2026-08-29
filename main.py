e8b7da9d3f3c6355422debb74cd95279d5a5b613
davidtav <david.mclaurel@outlook.com.br>
2026-08-29 20:29:16 -0300

    adiciona loop principal e atualiza a tela do jogo

--- main.py
+++ main.py
@@ -9,11 +9,17 @@ janela = pygame.display.set_mode((largura,altura))
 
 pygame.display.set_caption("Tetris Python")
 
+clock = pygame.time.Clock()
+
 rodando = True
 
 while rodando:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             rodando = False
+
+    janela.fill((20,20,20))
+    pygame.display.update()
+    clock.tick(60)        
 
 pygame.quit()
